ebe1510bc71bc9b5d765a7c19baddecd434b921a
Surag11-favor <your-github-kwamesuccess11@gmail.com>
2026-03-25 20:50:42 +0000

    FINAL: Relocated scanner and stabilized Investigator IAM

--- run_app.bat
+++ run_app.bat
@@ -5,5 +5,4 @@ set "MYSQL_URL=jdbc:mysql://localhost:3306/frauddb?createDatabaseIfNotExist=true
 set "MYSQLUSER=root"
 set "MYSQLPASSWORD="
 set "PORT=8080"
-"C:\Program Files\Java\jdk-25\bin\java.exe" -jar "target/fraud-intel-0.0.1-SNAPSHOT.jar"
-
+"%JAVA_HOME%\bin\java.exe" -Dspring.profiles.active=dev -jar target/fraud-intel-0.0.1-SNAPSHOT.jar
